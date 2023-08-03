7a83ec37dd4c7ff68591861fa5e094e3e51d63e9
Frantz Alexander <mr.frantz.alexander@gmail.com>
2023-08-03 14:36:38 -0400

    fixed bug with paddles

--- paddle.py
+++ paddle.py
@@ -19,5 +19,5 @@ class Paddle(Turtle):
         self.goto(self.xcor(), new_y)
 
     def go_down(self):
-        new_y = self.ycorr() - 20
+        new_y = self.ycor() - 20
         self.goto(self.xcor(), new_y)
