4cf7e4d04da1a559a25ffa816de38e9f0c7b27d4
Frantz Alexander <mr.frantz.alexander@gmail.com>
2023-08-03 11:09:25 -0400

    updated paddles in the main game file

--- main.py
+++ main.py
@@ -11,23 +11,16 @@ screen.setup(
 screen.title("Pong")
 screen.tracer(0)
 
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(
-    stretch_wid = 5,
-    stretch_len = 1
-)
-paddle.penup()
-paddle.goto(350, 0)
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
+    new_y = Paddle.ycor() + 20
+    Paddle.goto(Paddle.xcor(), new_y)
 
 def go_down():
-    new_y = paddle.ycorr() - 20
-    paddle.goto(paddle.xcor(), new_y)
+    new_y = Paddle.ycorr() - 20
+    Paddle.goto(Paddle.xcor(), new_y)
 
 screen.listen()
 screen.onkey(go_up, "Up")
